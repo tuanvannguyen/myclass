ae4393b3cc6120b60b7b43d0ff675a0d18b25803
ThanhNam <namnguyenthanh024@gmail.com>
2016-12-21 15:20:49 +0700

    Bo sung lai thong tin thu muc ThanhNam

--- ThanhNam/js/controler.js
+++ ThanhNam/js/controler.js
@@ -7,28 +7,20 @@ function clickComment(){
 		//lay noi dung comment
 		var content=$('#text-comment').val();
 		//cat lay chu dau cua ten
-		var fisrtName=name.substring(0,1);
+		var fisrtName=name.substring(0,1).toLowerCase();
 		
-		
-		//kiem tra thong tin co bi trung hay chua
-		
-		var oldFirstName=$('#name-avata').html();
-		console.log(oldFirstName);
-	
 		//tao div de add thong tin
 		
 		var myDiv='<div class=people-comment>';
-		    myDiv+='<div class="avata value-'+ fisrtName  +'">';
-		    myDiv+='<p id=name-avata>'+fisrtName+'</p>';
+		    myDiv+='<div class=avata>';
+		    myDiv+='<image src=image/'+fisrtName+'.png/>';
 			myDiv+='</div>';
 			myDiv+='<div class=info-people>';
 		    myDiv+='<p id=name>'+name+'</p>';
 			myDiv+='<p id=name>'+content+'</p>';
 			myDiv+='</div>';
 			$('.info').append(myDiv);
-		    var back =["#ff4000","#80ff00","#00ff00","#00ffff","#bf00ff","#000080","#0000FF","#228B22"];
-		    var rand = back[Math.floor(Math.random() * back.length)];
-		    $('.value-'+fisrtName+'').css('background',rand);
+			
 	});
 }
 
